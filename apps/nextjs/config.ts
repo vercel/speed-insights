a2ea60a960f713d00f834e649b98e6a2c6f62abe
Tobias Lins <me@tobi.sh>
2023-09-07 14:08:58 +0200

    Update config.ts

--- apps/nextjs/config.ts
+++ apps/nextjs/config.ts
@@ -1,2 +1,4 @@
 export const SPEED_INSIGHTS_ID =
-  process.env.VERCEL_SPEED_INSIGHTS_ID || process.env.VERCEL_ANALYTICS_ID || '';
+  process.env.NEXT_PUBLIC_VERCEL_SPEED_INSIGHTS_ID ||
+  process.env.NEXT_PUBLIC_VERCEL_ANALYTICS_ID ||
+  '';
