5a6c83869efd49ec6820d59c3d31f746d0c9d380
Tobias Lins <me@tobi.sh>
2023-09-13 08:18:42 +0200

    Improve postinstall script

--- packages/web/scripts/postinstall.js
+++ packages/web/scripts/postinstall.js
@@ -1,14 +1,14 @@
-/* eslint-disable -- We want to log here */
-import fs from 'fs';
-import path from 'path';
+/* eslint-disable no-undef -- Allow the use `process` */
+/* eslint-disable no-console -- Allow the use of `console` */
+import fs from 'node:fs';
+import path from 'node:path';
 
 const customPath = process.argv[2];
+const projectDir = customPath || path.resolve(process.cwd());
 
-// Check .env.local for the presence of the analyticsId key
-function checkAnalyticsIdInEnv() {
+// Check .env files for the presence of the analyticsId key
+function findEnvFileWithAnalyticsId() {
   const files = ['.env.local', '.env.development.local', '.env'];
-  const projectDir = customPath || path.resolve(process.cwd());
-
   const envFile = files.find((file) => {
     const envPath = path.join(projectDir, file);
     if (!fs.existsSync(envPath)) {
@@ -23,31 +23,27 @@ function checkAnalyticsIdInEnv() {
   return envFile;
 }
 
-function isAnalyticsIdInNextConfig() {
-  const projectDir = customPath || path.resolve(process.cwd());
-  const jsConfigPath = path.join(projectDir, 'next.config.js');
-  const mjsConfigPath = path.join(projectDir, 'next.config.mjs');
+function isSpeedInsightsInstalled() {
   const packageJsonPath = path.join(projectDir, 'package.json');
 
   if (!fs.existsSync(packageJsonPath)) {
     console.error('Error: package.json not found in the current directory.');
-    return;
+    return false;
   }
 
   const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf-8');
   const packageJson = JSON.parse(packageJsonContent);
 
   if (!packageJson.dependencies) {
-    return;
+    return false;
   }
 
-  const hasSpeedInsightsInstalled =
-    packageJson.dependencies['@vercel/speed-insights'];
+  return Boolean(packageJson.dependencies['@vercel/speed-insights']);
+}
 
-  if (!hasSpeedInsightsInstalled) {
-    // Has no speed-insights installed, so no need to check for analyticsId
-    return;
-  }
+function isAnalyticsIdInNextConfig() {
+  const jsConfigPath = path.join(projectDir, 'next.config.js');
+  const mjsConfigPath = path.join(projectDir, 'next.config.mjs');
 
   let configFile;
 
@@ -64,18 +60,27 @@ function isAnalyticsIdInNextConfig() {
   return configContent.includes('analyticsId');
 }
 
-const isInConfig = isAnalyticsIdInNextConfig();
-const envFile = checkAnalyticsIdInEnv();
+function main() {
+  if (!isSpeedInsightsInstalled()) {
+    // No @vercel/speed-insights installed, we don't need to continue
+    return;
+  }
 
-if (isInConfig) {
-  console.warn(
-    '\x1b[31m',
-    `Please remove 'analyticsId' from your next.config.js file.`,
-  );
+  const isInConfig = isAnalyticsIdInNextConfig();
+  const envFile = findEnvFileWithAnalyticsId();
+
+  if (isInConfig) {
+    console.warn(
+      '\x1b[31m',
+      `Please remove 'analyticsId' from your next.config.js file.`,
+    );
+  }
+  if (envFile) {
+    console.log(
+      '\x1b[31m',
+      `Please remove 'VERCEL_ANALYTICS_ID' from your ${envFile} file.`,
+    );
+  }
 }
-if (envFile) {
-  console.log(
-    '\x1b[31m',
-    `Please remove 'VERCEL_ANALYTICS_ID' from your ${envFile} file.`,
-  );
-}
+
+main();
