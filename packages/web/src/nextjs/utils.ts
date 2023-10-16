88c62de7b13428fa9a5ccd232aee47f33d573bf8
Tobias Lins <me@tobi.sh>
2023-10-16 13:43:13 +0200

    Fix pages router detection

--- packages/web/src/nextjs/utils.ts
+++ packages/web/src/nextjs/utils.ts
@@ -1,10 +1,20 @@
 'use client';
-import { useParams, usePathname } from 'next/navigation';
+import { useParams, usePathname, useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 import { computeRoute } from '../utils';
 
 export const useRoute = (): string | null => {
   const params = useParams();
+  const searchParams = useSearchParams();
   const path = usePathname();
 
-  return computeRoute(path, params);
+  const merged = useMemo(
+    () => ({
+      ...params,
+      ...Object.fromEntries(searchParams.entries()),
+    }),
+    [params, searchParams],
+  );
+
+  return computeRoute(path, merged);
 };
