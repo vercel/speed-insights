f2f74d5e2ece1d60cbdecf78aa6881db63590a27
Tobias Lins <me@tobi.sh>
2023-10-18 09:37:39 +0200

    Add example svelte project

---
+++ apps/svelte/src/lib/analytics.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { inject } from '@vercel/speed-insights';
+
+  const speedInsights = inject({ route: $page.route.id });
+
+  $: {
+    if (speedInsights && $page.route.id) {
+      speedInsights.setRoute($page.route.id);
+    }
+  }
+</script>
