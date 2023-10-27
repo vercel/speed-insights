b3ad8569dbe27f2eb17cda3beba7e816c312ec81
Tobias Lins <me@tobi.sh>
2023-10-27 22:16:31 +0200

    Use suspense for Next.js component to avoid client-side rendering (#14)

--- packages/web/src/nextjs/index.tsx
+++ packages/web/src/nextjs/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { SpeedInsights as SpeedInsightsScript } from '../react';
 import type { SpeedInsightsProps } from '../types';
 import { useRoute } from './utils';
 
-export function SpeedInsights(
-  props: Omit<SpeedInsightsProps, 'route'>,
-): JSX.Element {
+type Props = Omit<SpeedInsightsProps, 'route'>;
+
+function SpeedInsightsComponent(props: Props): React.ReactElement {
   const route = useRoute();
 
   return <SpeedInsightsScript route={route} {...props} framework="next" />;
+}
+
+export function SpeedInsights(props: Props): React.ReactElement {
+  return (
+    <Suspense fallback={null}>
+      <SpeedInsightsComponent {...props} />
+    </Suspense>
+  );
 }
