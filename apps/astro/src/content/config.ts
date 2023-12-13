7c69d23c3f89d408d372188f832439bdc21c58e2
Damien Simonin Feugas <damien@vercel.com>
2023-12-13 15:52:41 +0100

    feat: Astro support (#29)
    Co-authored-by: Tobias Lins <me@tobi.sh>

---
+++ apps/astro/src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = { blog };
