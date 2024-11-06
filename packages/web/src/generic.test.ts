1d1845e993527c82908f4d2714e55ef8407532c1
Damien Simonin Feugas <damien@vercel.com>
2024-11-06 13:07:22 +0100

    fix: utm parameters takes precedence over route params (#82)

--- packages/web/src/generic.test.ts
+++ packages/web/src/generic.test.ts
@@ -1,9 +1,10 @@
+import { describe, it, expect } from '@jest/globals';
 import { injectSpeedInsights } from './generic';
 
 describe('injectSpeedInsights()', () => {
   it('allows no parameters', () => {
     expect(injectSpeedInsights()).toEqual({
-      setRoute: expect.any(Function) as () => void,
+      setRoute: expect.any(Function),
     });
     expectInjectedScript();
   });
@@ -11,7 +12,7 @@ describe('injectSpeedInsights()', () => {
   it('can set framework', () => {
     const framework = 'sveltekit';
     expect(injectSpeedInsights({ framework })).toEqual({
-      setRoute: expect.any(Function) as () => void,
+      setRoute: expect.any(Function),
     });
     expectInjectedScript({ framework });
   });
@@ -21,13 +22,11 @@ function expectInjectedScript({
   src = 'https://va.vercel-scripts.com/v1/speed-insights/script.debug.js',
   framework = '',
 } = {}): void {
-  const candidateScript = document.querySelector('script');
-  expect(candidateScript).toBeDefined();
-  /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- the previous assertion fails otherwise */
-  const script = candidateScript!;
-  expect(script.defer).toBe(true);
-  expect(script.src).toBe(src);
-  expect(script.dataset.sdkn).toBe(
+  const script = document.querySelector('script');
+  expect(script).toBeDefined();
+  expect(script?.defer).toBe(true);
+  expect(script?.src).toBe(src);
+  expect(script?.dataset.sdkn).toBe(
     `@vercel/speed-insights${framework ? `/${framework}` : ''}`,
   );
 }
