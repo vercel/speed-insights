698960a1c2a97e59d28825effaff86fc3da4a24c
feugy <damien@vercel.com>
2024-11-22 09:01:02 +0100

    chore: handles PR remarks

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -1,5 +1,3 @@
-// import { cssBundleHref } from '@remix-run/css-bundle';
-import type { LinksFunction } from '@remix-run/node';
 import {
   Links,
   Meta,
@@ -8,10 +6,6 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import { SpeedInsights } from '@vercel/speed-insights/remix';
-
-// export const links: LinksFunction = () => [
-//   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
-// ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
