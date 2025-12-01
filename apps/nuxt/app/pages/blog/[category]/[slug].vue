f48623c059cd7cab3919c3f7ecb8a109d4f886d9
Hugo <hugo.richard@vercel.com>
2025-12-01 09:19:42 +0000

    feat(nuxt): Add support for `injectSpeedInsights()` and Nuxt module (#104)
    Co-authored-by: Sébastien Chopin <atinux@gmail.com>

--- apps/nuxt/app/pages/blog/[category]/[slug].vue
+++ apps/nuxt/app/pages/blog/[category]/[slug].vue
@@ -5,6 +5,5 @@ const route = useRoute();
 <template>
   <div class="greetings">
     <h1 class="green">{{ route.params.slug }}!</h1>
-    <h3>Category {{ route.params.category }}</h3>
   </div>
 </template>
