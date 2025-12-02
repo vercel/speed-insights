2452c35b7f11fe1d1a850b5527375e2a6bd574c1
Hugo <hugo.richard@vercel.com>
2025-12-02 13:06:24 +0000

    fix(nuxt): remove the module as breaking change (#107)

--- apps/nuxt/pages/blog/[category]/[slug].vue
+++ apps/nuxt/pages/blog/[category]/[slug].vue
@@ -5,5 +5,6 @@ const route = useRoute();
 <template>
   <div class="greetings">
     <h1 class="green">{{ route.params.slug }}!</h1>
+    <h3>Category {{ route.params.category }}</h3>
   </div>
 </template>
