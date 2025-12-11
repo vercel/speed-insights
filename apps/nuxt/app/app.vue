fccc01fed7872100d9f8ebc84f677ef62d468d36
Hugo Richard <hugo.richard@vercel.com>
2025-12-11 14:58:17 +0000

    feat(nuxt): Add support for `injectSpeedInsights()` and Nuxt module

---
+++ apps/nuxt/app/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/**
+ * Uncomment only when disabling the module in nuxt.config.ts
+ * As well as using the deprected <SpeedInsights> component
+ * We recommend using the module or the injectSpeedInsights function in a Nuxt plugin
+ */
+// import { SpeedInsights, type BeforeSendMiddleware } from '@vercel/speed-insights/nuxt';
+
+// const beforeSend = (event: BeforeSendMiddleware) => {
+//   console.log('Sending event:', event);
+//   return event;
+// };
+</script>
+
+<template>
+  <!-- <SpeedInsights :before-send="beforeSend" /> -->
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
