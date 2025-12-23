1d4f2035cbc46b7ab9266f7b7f79f744cd38998e
Hugo <hugo.richard@vercel.com>
2025-12-23 10:28:55 +0100

    feat(nuxt)!: Add support for `injectSpeedInsights()` and Nuxt module (#110)

--- packages/web/src/nuxt/index.ts
+++
@@ -1,4 +0,0 @@
-import { createComponent } from '../vue/create-component';
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- vue's defineComponent return type is any
-export const SpeedInsights = createComponent('nuxt');
