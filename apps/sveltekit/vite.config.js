79677584a80d0727096e29851b5065bf84c72953
Tee Ming Chew <chewteeming01@gmail.com>
2026-06-24 15:57:56 +1000

    migrate to svelte 5 anad kit 3

--- apps/sveltekit/vite.config.js
+++ apps/sveltekit/vite.config.js
@@ -1,6 +1,11 @@
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit({
+      adapter: adapter(),
+    }),
+  ],
 });
--- packages/web/src/sveltekit/index.svelte.ts
+++ packages/web/src/sveltekit/index.svelte.ts
@@ -1,7 +1,5 @@
 import type {} from '@sveltejs/kit'; // don't remove, ensures ambient types for $app/* are loaded
-import { get } from 'svelte/store';
-import { browser } from '$app/environment';
-import { page } from '$app/stores';
+import { page } from '$app/state';
 import { injectSpeedInsights as inject } from '../generic';
 import type { BeforeSend, BeforeSendEvent, SpeedInsightsProps } from '../types';
 import { getBasePath, getConfigString } from './utils';
@@ -9,24 +7,24 @@ import { getBasePath, getConfigString } from './utils';
 export function injectSpeedInsights(
   props: Omit<SpeedInsightsProps, 'framework'> = {},
 ): void {
-  if (browser) {
-    const speedInsights = inject(
+  let speedInsights: ReturnType<typeof inject>;
+
+  $effect.pre(() => {
+    if (speedInsights) return;
+
+    speedInsights = inject(
       {
-        route: get(page).route?.id,
+        route: page.route?.id,
         ...props,
         framework: 'sveltekit',
         basePath: getBasePath(),
       },
       getConfigString(),
     );
+  });
 
-    if (speedInsights) {
-      page.subscribe(({ route }) => {
-        if (route?.id) {
-          speedInsights.setRoute(route.id);
-        }
-      });
-    }
-  }
+  $effect.pre(() => {
+    speedInsights?.setRoute(page.route.id);
+  });
 }
 export type { SpeedInsightsProps, BeforeSend, BeforeSendEvent };
